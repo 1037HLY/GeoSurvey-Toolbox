bf832a19ed2808b93fe1dfc5172d886cb75878a8
1037HLY <hs202027323@gmail.com>
2026-08-01 09:56:50 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/geosurvey/MainActivity.kt
@@ -0,0 +1,32 @@
+// GeoSurvey-Toolbox/app/src/main/java/com/geosurvey/MainActivity.kt
+package com.geosurvey
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.geosurvey.ui.theme.GeoSurveyTheme
+import com.geosurvey.ui.screens.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeoSurveyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
